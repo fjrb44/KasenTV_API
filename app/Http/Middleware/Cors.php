407923fcc326bdcedeb8a12b6f1686c6fa7c2d46
Francisco Javier Rios Bautista <fjrb44@gmail.com>
2019-06-14 09:16:12 +0200

    Arreglado el fallo del post
    Se ha arreglado el fallo que pasaba con el post que te devolvia la pagina web de inicio de laravel en vez de hacer un post.

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -14,10 +14,6 @@ class Cors
      * @return mixed
      */
     public function handle($request, Closure $next) {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', "Origin, Content-type, X-auth-Token, Authorization")
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request);
     }
 }
